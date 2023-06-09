bca79c11035c1f61e140dcd4e79f50251474e8a7
HeitorNOC <heiitornoc@gmail.com>
2023-06-09 01:36:09 -0300

    navbar media queries

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,44 +1,103 @@
-import { Button,  NavContainer, NavContent, NavLeft, NavRight } from "./styles";
-import Logo from '../../../public/assets/images/Colibri.svg'
+import { useState, useEffect } from "react";
+import {
+  Button,
+  CloseButton,
+  NavContainer,
+  NavContent,
+  NavLeft,
+  NavRight,
+  SidebarIcon,
+  SidebarLinks,
+} from "./styles";
+import Logo from "../../../public/assets/images/Colibri.svg";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { SignIn, SignOut } from "@phosphor-icons/react";
+import { SignIn, SignOut, List } from "@phosphor-icons/react";
 
 export default function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   function handleSignIn() {
-    signIn('google')
+    signIn("google");
   }
 
   function handleSignOut() {
-    signOut()
+    signOut();
   }
 
-  const session = useSession()
+  const session = useSession();
+
+  function toggleSidebar() {
+    setShowSidebar(!showSidebar);
+  }
 
   return (
     <>
-      <NavContainer>
+      <NavContainer className={showSidebar ? "blur" : ""}>
         <NavContent>
-
-        <NavLeft>
-          <Image src={Logo} alt="logo" />
-        </NavLeft>
-        <NavRight>
-          <a href="#inicio">Início</a>
-          <a href="#bio">Biografia</a>
-          <a href="#cordeis">Cordéis</a>
-          <a href="#livros">Livros</a>
-          <a href="#videos">Vídeos</a>
-          <a href="#audios">Áudios</a>
-          <Button color={"white"}>Contato</Button>
-          {
-            session.data ? <Button onClick={handleSignOut} >{session.data.user.name.split(' ', 1)} <SignOut color="#FFF"/></Button> : <Button onClick={handleSignIn}>Login<SignIn color="#FFF"/></Button>
-          }
-          
-        </NavRight>
+          <NavLeft>
+            <Image src={Logo} alt="logo" />
+          </NavLeft>
+          {windowWidth >= 769 ? (
+            <NavRight>
+              <a href="#inicio">Início</a>
+              <a href="#bio">Biografia</a>
+              <a href="#cordeis">Cordéis</a>
+              <a href="#livros">Livros</a>
+              <a href="#videos">Vídeos</a>
+              <a href="#audios">Áudios</a>
+              <Button color={"white"}>Contato</Button>
+              {session.data ? (
+                <Button onClick={handleSignOut}>
+                  {session.data.user.name.split(" ", 1)}{" "}
+                  <SignOut color="#FFF" />
+                </Button>
+              ) : (
+                <Button onClick={handleSignIn}>
+                  Login<SignIn color="#FFF" />
+                </Button>
+              )}
+            </NavRight>
+          ) : (
+            <SidebarIcon onClick={toggleSidebar}>
+              <List size={32} color="#000" />
+            </SidebarIcon>
+          )}
         </NavContent>
-        
       </NavContainer>
+      {showSidebar && windowWidth < 769 && (
+        <>
+          <SidebarLinks>
+            <a href="#inicio">Início</a>
+            <a href="#bio">Biografia</a>
+            <a href="#cordeis">Cordéis</a>
+            <a href="#livros">Livros</a>
+            <a href="#videos">Vídeos</a>
+            <a href="#audios">Áudios</a>
+            <Button color={"white"}>Contato</Button>
+            {session.data ? (
+              <Button onClick={handleSignOut}>
+                {session.data.user.name.split(" ", 1)} <SignOut color="#FFF" />
+              </Button>
+            ) : (
+              <Button onClick={handleSignIn}>
+                Login<SignIn color="#FFF" />
+              </Button>
+            )}
+          </SidebarLinks>
+          <CloseButton onClick={toggleSidebar}>X</CloseButton>
+        </>
+      )}
     </>
-  )
+  );
 }
--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -1,28 +1,58 @@
 import { styled } from "@stitches/react";
 
 export const NavContainer = styled("div", {
-  
-
   background: "rgba(255, 255, 255, 0.8)",
   margin: "8px 240px",
   backdropFilter: "blur(10px)",
   borderRadius: "8px",
-  //padding: "16px",
   boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-})
+
+  "@media (max-width: 1440px)": {
+    margin: "8px 100px",
+  },
+
+  "@media (max-width: 1024px)": {
+    margin: "8px 20px",
+  },
+
+  "@media (max-width: 768px)": {
+    margin: "8px 20px",
+  },
+
+  "@media (max-width: 500px)": {
+    margin: "8px 10px",
+  },
+
+  variants: {
+    blur: {
+      true: {
+        filter: "blur(4px)",
+        pointerEvents: "none",
+      },
+    },
+  },
+});
 
 export const NavContent = styled("div", {
   display: "flex",
   alignItems: "center",
   justifyContent: "space-between",
-  padding: "8px 80px"
-  
+  padding: "8px 80px",
 
-  
-})
-export const NavLeft = styled("div", {
+  "@media (max-width: 1024px)": {
+    padding: "8px 20px",
+  },
 
-})
+  "@media (max-width: 768px)": {
+    padding: "8px 20px",
+  },
+
+  "@media (max-width: 500px)": {
+    padding: "8px 10px",
+  },
+});
+
+export const NavLeft = styled("div", {});
 
 export const Button = styled("button", {
   display: "flex",
@@ -31,8 +61,7 @@ export const Button = styled("button", {
   padding: "8px 20px",
   cursor: "pointer",
   background: "linear-gradient(to bottom, #4dc7d9 0%,#66a6ff 100%)",
-  
-      boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.2)",
+  boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.2)",
   border: "none",
   color: "#FFFFFF",
   fontWeight: 400,
@@ -42,34 +71,90 @@ export const Button = styled("button", {
   transition: "all 0.3s",
 
   "&:hover": {
-    transform: 'translateY(-3px)',
-        boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.3)",
+    transform: "translateY(-3px)",
+    boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.3)",
   },
+
   variants: {
     color: {
-      black:{
-        /* padding: "8px 14px",
-        background: "#000000",
-        color: "#FFFFFF",
-        fontWeight: 400,
-        fontSize: 20,
-        lineHeight: "150%", */
-      },
-      white: {
-        /* background: "#FFFFFF",
-        color: "#000000",
-        fontWeight: 400,
-        fontSize: 20,
-        lineHeight: "150%", */
-      }
-    }
-  }
-})
+      black: {},
+      white: {},
+    },
+  },
+
+  "@media (max-width: 500px)": {
+    //display: "none",
+  },
+});
+
+export const SidebarIcon = styled("div", {
+  display: "none",
+  cursor: "pointer",
+  "@media (max-width: 768px)": {
+    display: "flex",
+    alignItems: "center",
+    gap: 6,
+    padding: "8px 10px",
+  },
+});
+
+export const SidebarLinks = styled("div", {
+  display: "none",
+  flexDirection: "column",
+  gap: 16,
+  padding: "16px 24px",
+  position: "fixed",
+  top: 0,
+  right: 0,
+  width: "300px",
+  height: "100vh",
+  background: "rgba(255, 255, 255, 0.8)",
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  borderRadius: "0 0 8px 0",
+  overflowY: "auto",
+  zIndex: 999,
+
+  a: {
+    color: "#333333",
+    textDecoration: "none",
+    fontWeight: 500,
+    fontSize: 16,
+    lineHeight: "150%",
+    transition: "color 0.3s",
+
+    "&:hover": {
+      color: "#4dc7d9",
+    },
+  },
+
+  "@media (max-width: 768px)": {
+    display: "flex",
+  },
+});
+
+export const CloseButton = styled("button", {
+  display: "none",
+  cursor: "pointer",
+  background: "none",
+  border: "none",
+  color: "#000",
+  position: "fixed",
+  top: 20,
+  right: 20,
+  fontSize: 24,
+  padding: 8,
+  zIndex: 999999999999,
+
+  "@media (max-width: 768px)": {
+    display: "block",
+  },
+});
 
 export const NavRight = styled("div", {
   display: "flex",
   alignItems: "center",
   gap: 32,
+
   a: {
     textDecoration: "none",
     fontWeight: 400,
@@ -82,5 +167,17 @@ export const NavRight = styled("div", {
       color: "#555555",
     },
   },
-})
 
+  variants: {
+    blur: {
+      true: {
+        filter: "blur(4px)",
+        pointerEvents: "none",
+      },
+    },
+  },
+
+  "@media (max-width: 768px)": {
+    display: "none",
+  },
+});
